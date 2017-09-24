96baf3a2877a078f2d6b2d881677587cb357f5ab
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-09-24 04:43:33 -0700

    add triangle

--- resources/assets/js/components/DataTable.vue
+++ resources/assets/js/components/DataTable.vue
@@ -8,7 +8,13 @@
                     <thead>
                     <tr>
                         <th v-for="column in response.displayableColumns">
-                            <span @click="sortBy(column)">{{ column }}</span>
+                            <div class="sortable" @click="sortBy(column)">{{ column }}
+                                <div
+                                    class="arrow"
+                                    v-if="sort.key === column"
+                                    :class="{ 'arrow--asc': sort.order === 'asc', 'arrow--desc': sort.order === 'desc' }"
+                                ></div>
+                            </div>
                         </th>
                     </tr>
                     </thead>
@@ -74,3 +80,31 @@
         }
     }
 </script>
+
+
+<style lang="scss">
+    .sortable {
+        cursor: pointer;
+    }
+
+    .arrow {
+        display: inline-block;
+        vertical-align: middle;
+        width: 0;
+        height: 0;
+        margin-left: 5px;
+        opacity: .6;
+
+        &--asc {
+            border-left: 4px solid transparent;
+            border-right: 4px solid transparent;
+            border-bottom: 4px solid #222;
+        }
+
+        &--desc {
+            border-left: 4px solid transparent;
+            border-right: 4px solid transparent;
+            border-top: 4px solid #222;
+        }
+    }
+</style>
