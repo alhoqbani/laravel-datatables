6351be10dc8fbf7d4a842cf1c4ab124c33a5da5c
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-09-24 02:55:54 -0700

    add DataTable vue component

---
+++ resources/assets/js/components/DataTable.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-heading">Table Name</div>
+
+        <div class="panel-body">
+            Table
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'data-table',
+        props: ['endpoint'],
+        data () {
+            return {
+                response: {
+                    table: '',
+                    displayableColumns: [],
+                    records: [],
+                }
+            }
+        },
+        mounted() {
+            axios.get(`${this.endpoint}`).then(response => this.response = response);
+        }
+    }
+</script>
